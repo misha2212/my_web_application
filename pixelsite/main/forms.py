f9d1fadc4d594d33ac80dd6692a2356f3436b3cc
misha2212 <88970804+misha2212@users.noreply.github.com>
2022-01-14 15:32:51 +0700

    add via upload

--- pixelsite/main/forms.py
+++ pixelsite/main/forms.py
@@ -2,10 +2,6 @@ from django import forms
 from .models import *
 
 
-# class AddPostForm(forms.Form):
-#     title = forms.CharField(max_length=255)
-
-
 class AddPostForm(forms.ModelForm):
     class Meta:
         model = BaseImages
